1a2d53ae5904f54fdf3cb0824d364bb65fb21639
Radek Czemerys <radko93@gmail.com>
2017-08-02 23:39:02 +0200

    Fix typo in comment

--- android/src/main/java/com/cboy/rn/splashscreen/SplashScreenReactPackage.java
+++ android/src/main/java/com/cboy/rn/splashscreen/SplashScreenReactPackage.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 public class SplashScreenReactPackage implements ReactPackage {
 
-    // Deprecated RN 0.47
+    // Depreciated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
