df2f4a220aa80d3d05c5700206c593468a9ebfa2
Radek Czemerys <radko93@gmail.com>
2017-08-02 19:05:27 +0200

    Remove only @Override marker

--- android/src/main/java/com/cboy/rn/splashscreen/SplashScreenReactPackage.java
+++ android/src/main/java/com/cboy/rn/splashscreen/SplashScreenReactPackage.java
@@ -18,6 +18,10 @@ import java.util.List;
  * Email:crazycodeboy@gmail.com
  */
 public class SplashScreenReactPackage implements ReactPackage {
+    
+    public List<Class<? extends JavaScriptModule>> createJSModules() {		
+        return Collections.emptyList();		
+    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
