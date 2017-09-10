91536255c92fd0dd0e2782c97999d48db775bb0f
crazycodeboy <crazycodeboy@gmail.com>
2017-09-10 21:18:55 +0800

    Revert: update examples

--- examples/index.android.js
+++ examples/index.android.js
@@ -1,53 +1,17 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * SplashScreen
+ * 启动屏
+ * from：http://www.devio.org
+ * Author:CrazyCodeBoy
+ * GitHub:https://github.com/crazycodeboy
+ * Email:crazycodeboy@gmail.com
  * @flow
  */
 
-import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
 } from 'react-native';
 
-export default class examples extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import index from './index.js'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('examples', () => examples);
+AppRegistry.registerComponent('examples', () => index);
