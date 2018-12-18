d0e3898b1580854f37d7604dc4941c61c489bec0
Chet Corcos <ccorcos@gmail.com>
2018-12-17 17:10:46 -0800

    Better iOS launch screen logic.

--- ios/RNSplashScreen.h
+++ ios/RNSplashScreen.h
@@ -1,16 +1,9 @@
-/**
- * SplashScreen
- * 启动屏
- * from：http://www.devio.org
- * Author:CrazyCodeBoy
- * GitHub:https://github.com/crazycodeboy
- * Email:crazycodeboy@gmail.com
- */
 #import <React/RCTBridgeModule.h>
 #import <UIKit/UIKit.h>
 
-@interface RNSplashScreen : NSObject<RCTBridgeModule>
-+ (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView;
-+ (void)show;
-+ (void)hide;
+@interface RNSplashScreen : NSObject <RCTBridgeModule>
+@property(nonatomic, strong) UIViewController *launchScreen;
+- (void)show;
+- (void)hide;
+- (void)setup;
 @end
