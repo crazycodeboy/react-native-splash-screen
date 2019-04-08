86e4b31d064e834d10a5d2ae2752d04ad8ebcbf0
Mark Vayngrib <mark@tradle.io>
2019-04-08 17:28:18 -0400

    cleanup

--- ios/RNSplashScreen.h
+++ ios/RNSplashScreen.h
@@ -10,7 +10,6 @@
 #import <UIKit/UIKit.h>
 
 @interface RNSplashScreen : NSObject<RCTBridgeModule>
-+ (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView;
 + (void)show;
 + (void)hide;
 @end
