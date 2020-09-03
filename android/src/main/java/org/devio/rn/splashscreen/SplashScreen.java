44639c408b7a4ee9d58587e51b3de18c2d8f89f0
MuhammadUsman <M.UsmanQau786@gmail.com>
2020-09-03 15:13:54 +0500

    Add multiple splash

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -2,6 +2,8 @@ package org.devio.rn.splashscreen;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 
 import java.lang.ref.WeakReference;
@@ -15,78 +17,103 @@ import java.lang.ref.WeakReference;
  * Email:crazycodeboy@gmail.com
  */
 public class SplashScreen {
-    private static Dialog mSplashDialog;
-    private static WeakReference<Activity> mActivity;
+  private static Dialog mSplashDialog;
+  private static WeakReference<Activity> mActivity;
+  private static final String SPLASH_INDEX_KEY = "SplashIndex";
+  private static final int[] LAYOUTS = {
+    R.layout.launch_screen,
+    R.layout.launch_screen_1,
+    R.layout.launch_screen_2,
+    R.layout.launch_screen_3,
+    R.layout.launch_screen_4,
+    R.layout.launch_screen_5,
+    R.layout.launch_screen_6,
+  };
 
-    /**
-     * 打开启动屏
-     */
-    public static void show(final Activity activity, final int themeResId) {
-        if (activity == null) return;
-        mActivity = new WeakReference<Activity>(activity);
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (!activity.isFinishing()) {
-                    mSplashDialog = new Dialog(activity, themeResId);
-                    mSplashDialog.setContentView(R.layout.launch_screen);
-                    mSplashDialog.setCancelable(false);
 
-                    if (!mSplashDialog.isShowing()) {
-                        mSplashDialog.show();
-                    }
-                }
-            }
-        });
-    }
-
-    /**
-     * 打开启动屏
-     */
-    public static void show(final Activity activity, final boolean fullScreen) {
-        int resourceId = fullScreen ? R.style.SplashScreen_Fullscreen : R.style.SplashScreen_SplashTheme;
-
-        show(activity, resourceId);
-    }
-
-    /**
-     * 打开启动屏
-     */
-    public static void show(final Activity activity) {
-        show(activity, false);
-    }
-
-    /**
-     * 关闭启动屏
-     */
-    public static void hide(Activity activity) {
-        if (activity == null) {
-            if (mActivity == null) {
-                return;
-            }
-            activity = mActivity.get();
+  /**
+   * 打开启动屏
+   */
+  public static void show(final Activity activity, final int themeResId) {
+    if (activity == null) return;
+    mActivity = new WeakReference<Activity>(activity);
+    activity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        if (!activity.isFinishing()) {
+          mSplashDialog = new Dialog(activity, themeResId);
+          int currentSplashIndex = getValue(activity);
+          mSplashDialog.setContentView(LAYOUTS[currentSplashIndex]);
+          mSplashDialog.setCancelable(false);
+          if (!mSplashDialog.isShowing()) {
+            mSplashDialog.show();
+          }
+          setValue(activity);
         }
+      }
+    });
+  }
 
-        if (activity == null) return;
+  public static int getValue(final Activity activity) {
+    SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+    return sharedPref.getInt(SPLASH_INDEX_KEY, 0);
+  }
 
-        final Activity _activity = activity;
 
-        _activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (mSplashDialog != null && mSplashDialog.isShowing()) {
-                    boolean isDestroyed = false;
+  public static void setValue(final Activity activity) {
+    SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPref.edit();
+    editor.putInt(SPLASH_INDEX_KEY, (getValue(activity) + 1) % 7);
+    editor.apply();
+  }
 
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                        isDestroyed = _activity.isDestroyed();
-                    }
+  /**
+   * 打开启动屏
+   */
+  public static void show(final Activity activity, final boolean fullScreen) {
+    int resourceId = fullScreen ? R.style.SplashScreen_Fullscreen : R.style.SplashScreen_SplashTheme;
 
-                    if (!_activity.isFinishing() && !isDestroyed) {
-                        mSplashDialog.dismiss();
-                    }
-                    mSplashDialog = null;
-                }
-            }
-        });
+    show(activity, resourceId);
+  }
+
+  /**
+   * 打开启动屏
+   */
+  public static void show(final Activity activity) {
+    show(activity, false);
+  }
+
+  /**
+   * 关闭启动屏
+   */
+  public static void hide(Activity activity) {
+    if (activity == null) {
+      if (mActivity == null) {
+        return;
+      }
+      activity = mActivity.get();
     }
+
+    if (activity == null) return;
+
+    final Activity _activity = activity;
+
+    _activity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        if (mSplashDialog != null && mSplashDialog.isShowing()) {
+          boolean isDestroyed = false;
+
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            isDestroyed = _activity.isDestroyed();
+          }
+
+          if (!_activity.isFinishing() && !isDestroyed) {
+            mSplashDialog.dismiss();
+          }
+          mSplashDialog = null;
+        }
+      }
+    });
+  }
 }
