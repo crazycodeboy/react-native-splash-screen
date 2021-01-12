f10c1e3ebf5543a93bcc95a532dabe93d55fcabe
Jaime Bernardo <jaime@janeasystems.com>
2021-01-12 09:44:59 +0000

    example: add Windows application
    Adds a react-native-windows application to the example.

--- examples/metro.config.js
+++ examples/metro.config.js
@@ -4,8 +4,20 @@
  *
  * @format
  */
+const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
+  resolver: {
+    blacklistRE: blacklist([
+      // This stops "react-native run-windows" from causing the metro server to crash if its already running
+      new RegExp(
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
+      /.*\.ProjectImports\.zip/,
+    ]),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
